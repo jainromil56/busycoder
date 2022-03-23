b9206f18594bb0a7a06bab28a92c486eb9f44c7b
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-03-23 16:51:59 +0530

    Displayed API JSON content on screen
    Using useEffect (for fetching data) and useState (for saving fetched data) , Converted blogs.js functions to asynchronous function so that it can  get time to save all data in JSON format, Also used promises

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,12 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs'
 
-export default function handler(req, res) {
-  console.log(res)
+// converted function to aynchronous
+export default async function handler(req, res) {
+  // console.log(res)
+
   //readdir for reading directory by nodeJs
   //readFile for reading file by nodeJs
-  fs.readdir("blogdata",'utf-8', (err, data)=>{
-    console.log(data)
-    res.status(200).json( data )
-  })
+
+  // used await function so that it can read one after another and can get time to read all files
+  let data = await fs.promises.readdir('blogdata')
+  let myfile
+  let allblogs = []
+  for(let index = 0; index < data.length; index++){
+    const item = data[index];
+    console.log(item)
+    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+    // myFile contains all data of blogs
+    allblogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json( allblogs )
+  // fs.readdir("blogdata",'utf-8', (err, data)=>{
+  //   console.log(data)
+  //   data.forEach((item) => {
+  //     console.log(item)
+  //     // readfile(path, callback)
+  //     fs.readFile(('blogdata/' + item), ()=>{
+
+  //     })
+  //   })
+  // })
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,42 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import styles from '../styles/blog.module.css'
 import Link from 'next/link'
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+  
+  //useEffect to fetch data and useState to save it 
+  useEffect(() => {
+    console.log("UseEffect is running")
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
+    }).then((parsed) =>{
+      console.log(parsed)
+      setBlogs(parsed)
+    })
+  },[])
   return (
     <div>
-      
       <main className={styles.main}>
-      <div className="blogs">
+        {blogs.map((blogitem) => {
+          return (
+            <div key={blogitem.slug} className={styles.blogItem}>
+              <Link href={`/blogpost/${blogitem.slug}`}>
+                <h3>{blogitem.title}</h3>
+              </Link>
+              <p>{blogitem.content.substr(0,200)}</p>
+            </div>
+          );
+        })}
+        <div className="blogs">
           {/* <h2>Popular Blogs</h2> */}
-          <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-javascript'}>
-            <h3>How to learn javascript in 2021?</h3> 
+
+          {/* <div className={styles.blogItem}>
+            <Link href={"/blogpost/learn-javascript"}>
+              <h3>How to learn javascript in 2021?</h3>
             </Link>
             <p>Javascript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-javascript'}>
-            <h3>How to learn javascript in 2021?</h3> 
-            </Link>
-            <p>Javascript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-javascript'}>
-            <h3>How to learn javascript in 2021?</h3> 
-            </Link>
-            <p>Javascript is the language used to design logic for the web</p>
-          </div>
-          <div className={styles.blogItem}>
-            <Link href={'/blogpost/learn-javascript'}>
-            <h3>How to learn javascript in 2021?</h3> 
-            </Link>
-            <p>Javascript is the language used to design logic for the web</p>
-          </div>
+          </div> */}
         </div>
-        </main>
+      </main>
     </div>
-  )
+  );
 }
 
 export default Blog
