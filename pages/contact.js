96fadb56930877737b14e3a2dc1ce69e6a09a923
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-03-24 18:27:09 +0530

    Made contact form
    Now when we click on submit in form, data gets store in form of json in contactdata folder, Every submit creates new file in contactdata folder.

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,127 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../styles/contact.module.css";
 
 const Contact = () => {
-  return (
-    <div>This is Contact page</div>
-  )
-}
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [phone, setphone] = useState("");
+  const [desc, setdesc] = useState("");
 
-export default Contact 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const submitapi = "http://localhost:3000/api/postcontact/";
+
+    let data = {
+      name: name,
+      email: email,
+      phone: phone,
+      desc: desc,
+    };
+
+    //for posting data to server now check contactdata file, new file will be created their
+    //after submitting
+    fetch(submitapi, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+          console.log("Success : ", data)
+          alert("Thanks for your submission")
+          //sets field back to empty
+          setname('')
+          setemail('')
+          setphone('')
+          setdesc('')
+      })
+      .catch((error) => {
+        console.error("Error : ", error);
+      });
+
+    console.log(name, email, phone, desc);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name == "name") {
+      setname(e.target.value);
+    } else if (e.target.name == "email") {
+      setemail(e.target.value);
+    } else if (e.target.name == "phone") {
+      setphone(e.target.value);
+    } else if (e.target.name == "desc") {
+      setdesc(e.target.value);
+    }
+    // console.log(e.target.value, "change")
+  };
+
+  return (
+    <div className={styles.container}>
+      <form id={styles.contact} action="" method="post" onSubmit={handleSubmit}>
+        <h3>Contact Us</h3>
+        <h4>Contact us for custom quote</h4>
+        <div>
+          <input
+            placeholder="Your name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            type="text"
+            required
+            autoFocus
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Your Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            type="email"
+            required
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Your Phone Number"
+            name="phone"
+            value={phone}
+            onChange={handleChange}
+            type="tel"
+            required
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Your Web Site (optional)"
+            name="website"
+            type="url"
+          />
+        </div>
+        <div>
+          <textarea
+            placeholder="Type your message here...."
+            name="desc"
+            value={desc}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <div>
+          <button
+            name="submit"
+            type="submit"
+            id="contact-submit"
+            data-submit="...Sending"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
