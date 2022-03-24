0d5db047eccb21ff434e0c709b44c5bd1f2e270d
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-03-24 19:08:58 +0530

    Designed about page and made some minor changes

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from '../styles/about.module.css'
 
 const about = () => {
   return (
-    <div>This is about page</div>
+    <div className={styles.container}>
+      <h3>About us</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate magni, mollitia odio consequatur aliquam ex sed dolore veniam asperiores vel similique cupiditate atque assumenda qui deserunt consectetur!</p>
+
+      <div className={styles.service}>
+        <h3>Services Offered</h3>
+        <ul>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+          <li>
+            <h5>lorem ipsum dolor</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo natus magnam tenetur quis? Autem suscipit deserunt nesciunt corrupti!</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
